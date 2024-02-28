c3c2512dd73c27804067a99e19de26ffbc134034
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-02-28 18:24:24 +0000

    added html

--- server.js
+++ server.js
@@ -1,74 +1,87 @@
 const puppeteer = require('puppeteer');
-const  logger = require( './api/logging/logger');
-const url = require ('url');
+const logger = require('./api/logging/logger');
 const http = require('http');
-const fs = require("fs");
-
-
+const fs = require('fs');
+const path = require('path');
+const { URL } = require('url');
+const {query} = require("winston");
+// Set up the server port
 const port = process.env.PORT || 3002;
 
+let defaultpath = "https://ddttom.global.ssl.fastly.net/article/13-must-have-features-in-a-content-management-system";
+
+defaultpath =  "https://ddttom.global.ssl.fastly.net/article/new-adobe-cms-a-faster-way-to-publish-content-but-at-what-cost";
+// Create the HTTP server
 http.createServer(async (req, res) => {
-    let HTMLurl = '';
-    let outName = 'mine.html';
-
     if (req.url.startsWith('/readHTML')) {
-        let queryObject;
-        queryObject = url.parse(req.url, true).query;
-        let queryValue = queryObject.path + "";
+        const baseURL = `http://${req.headers.host}/`; // Use http as we're not implementing https here
+        const myURL = new URL(req.url, baseURL);
+        let queryValue = myURL.searchParams.get('path');
 
-        if (queryValue.startsWith('/')) {
-            queryValue = queryValue.substring(1);
+        if (!queryValue) {
+            queryValue = defaultpath;
         }
 
-        if (!queryObject.path) {
-            res.writeHead(400, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify({message: 'Missing path query parameter'}));
-            return;
+        try {
+            const result = await scrape(queryValue);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("done");
+        } catch (error) {
+            logger.error(`Scraping error: ${error}`);
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Internal Server Error' }));
         }
-
-        HTMLurl = queryObject.path.toString();
     } else if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Welcome to the HTML Creator service.</h1><a href="/readHTML?path=https://ddttom.global.ssl.fastly.net/hometest.html">Click here to check an image</a>');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Welcome to the HTML Creator service.</h1><a href="/readHTML?path="' + defaultpath + '">Click here to start</a>');
     } else {
-        res.writeHead(404, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({message: 'Resource not found'}));
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Resource not found' }));
     }
-    await scrape(HTMLurl, outName);
-    res.end();
 }).listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/readHTML?path=https://ddttom.global.ssl.fastly.net/hometest.html`);
+    logger.info(`Server running on http://localhost:${port}/`+'readHTML?path=' + defaultpath);
 });
 
+function fixup(content) {
 
-logger.info(`Server running on http://localhost:${port}/readHTML?path=https://ddttom.global.ssl.fastly.net/hometest.html`);
+    let contentArray = content.split('\n');
+    for (let i = 0; i < contentArray.length; i++) {
+        contentArray[i] = contentArray[i].trimRight();
+    }
+    content = contentArray.join('\n');
+    while (content.includes('\n\n')) {
+        content = content.replaceAll('\n\n', '\n');
+    }
+    content = content.replace('<script type="application/ld+json"' ,'\n<script type="application/ld+json"' );
+    return content;
+}
 
-
-
-async function scrape(url,filePath) {
+// Puppeteer function to scrape HTML content
+async function scrape(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, {
-        waitUntil: 'networkidle2' // Waits for the network to be idle (no more than 2 network connections for at least 500 ms)
-    });
+    await page.goto(url, { waitUntil: 'networkidle2' });
 
-    // Wait for the necessary JavaScript to execute (optional, adjust as necessary)
-    // await page.waitForSelector('selector'); // Example: wait for a specific element to load
-
-    // Extract the DOM content
-    const content = await page.content(); // Gets the entire HTML contents of the page as a string
-
-    // Close the browser
+    const content = await page.content();
     await browser.close();
 
-    // save the DOM content
-    fs.writeFile(filePath, content, (err) => {
-        if (err) {
-            logger.crit(`Error writing file: ${err}`);
-        } else {
-            logger.info(`Saved DOM content to ${filePath}`);
-
-        }
+    const filePath = `${getLastPartOfPathWithoutExtension(url)}.html`; // Ensure extension is added
+    return new Promise((resolve, reject) => {
+        const fixedcontent = fixup(content);
+        fs.writeFile(filePath, fixedcontent, err => {
+            if (err) {
+                logger.error(`Error writing file: ${err}`);
+                reject(err);
+            } else {
+                logger.info(`Saved DOM content to ${filePath}`);
+                resolve(content); // Resolve with content instead of file path
+            }
+        });
     });
 }
 
+// Function to extract the last part of the path without the extension
+function getLastPartOfPathWithoutExtension(urlString) {
+    const parsedUrl = new URL(urlString), pathname = parsedUrl.pathname;
+    return path.basename(pathname, path.extname(pathname));
+}
